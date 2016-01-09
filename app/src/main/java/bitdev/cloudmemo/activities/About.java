6b2e9222b23287f70a15c23a36893fa65b03e7ef
Rizky Pratama Putra <rizky_scream@yahoo.com>
2016-01-09 13:27:30 -0800

    Create About.java

---
+++ app/src/main/java/bitdev/cloudmemo/activities/About.java
@@ -0,0 +1,17 @@
+package bitdev.cloudmemo.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import bitdev.cloudmemo.R;
+
+/**
+ * Created by Acer on 1/10/2016.
+ */
+public class About extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+    }
+}
